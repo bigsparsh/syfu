eeb5c400f202647cef0183b0d9ee9f58d0db0fc8
Sparsh Singh <sparshsingh7586@gmail.com>
2026-07-22 19:16:00 +0530

    experiment: worked out the basic rag

--- frontend/src/Landing.vue
+++ frontend/src/Landing.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="bg-black h-screen w-screen">
-        
-    </div>>
+	<div class="bg-white px-3 overflow-clip flex flex-col gap-2">
+		<div class="h-screen flex flex-col justify-center items-start text-white w-full space rounded-4xl
+			bg-radial-[at_50%_150%] from-stone-800 to-black relative">
+			<img src="/strokes.svg" class="absolute w-[80vw] self-center" />
+			<h1 class="text-[22.5vw] tracking-[5vw] sora font-black ml-[5vw] self-center leading-tight z-80">SYFU</h1>
+			<div class="px-[10vw] flex flex-col gap-3">
+				<h2 class="text-4xl">Take control of your time.</h2>
+				<button class="text-2xl font-black text-black px-5 py-2 bg-white w-fit">Get Started</button>
+			</div>
+		</div>
+		<div>hellow world</div>
+	</div>
 </template>
